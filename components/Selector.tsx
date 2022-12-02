37d39f99e887075ee4434d99c0a95437b0ba354c
igrimzyi <1isaiahofficial1@gmail.com>
2022-12-02 12:14:15 -0800

    New edits

---
+++ components/Selector.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+const Selecttor = () => {
+        return (
+            <View>
+                <Text>This would be selector</Text>
+            </View>
+        );
+};
