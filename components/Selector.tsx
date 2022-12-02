d1466c29c0ffa4b95b57c5e0dd939a35689c8882
igrimzyi <1isaiahofficial1@gmail.com>
2022-12-02 12:15:15 -0800

    Selector

--- components/Selector.tsx
+++ components/Selector.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
-const Selecttor = () => {
+const Selector = () => {
         return (
             <View>
                 <Text>This would be selector</Text>
             </View>
         );
 };
+
+export default Selector;
